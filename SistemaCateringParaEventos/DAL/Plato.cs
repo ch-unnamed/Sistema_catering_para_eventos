6a6c59415a5522344e258dbb40dcf90d57bb52ae
ch-unnamed <pepe.n.acuna@gmail.com>
2025-05-14 20:00:27 -0300

    CRUD de plato sin validaciones

---
+++ SistemaCateringParaEventos/DAL/Plato.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class Plato
+    {
+        public void Insertar(BE.Plato objPlato)
+        {
+
+        }
+
+        public void Editar(BE.Plato objPlato)
+        {
+
+        }
+
+        public void Eliminar(BE.Plato objPlato)
+        {
+
+        }
+
+        public BE.Plato Buscar(string NombrePlato)
+        {
+            BE.Plato platoEncontrado = new BE.Plato();
+            return platoEncontrado ;
+        }
+    }
+}
